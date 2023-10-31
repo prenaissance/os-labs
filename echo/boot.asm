44581f839c83c951ab45636ad47645c253a11034
prenaissance <alexandru.andries@isa.utm.md>
2023-10-31 23:56:13 +0200

    feat(lab2): implement all but backspace removal

--- echo/boot.asm
+++ echo/boot.asm
@@ -3,16 +3,22 @@ ORG 7c00H
 
 %define BACKSPACE 0x08
 %define ENTER 0x0D
+%define ESC 0x1B
 
 cursor_coords:
 cursor_x db 0
 cursor_y db 0
 
+row db 1
+
 buffer:
-resb 64
+resb 63
 
 start:
-    call clear_screen; clear the screen
+    mov ah, 0; set the video mode
+    mov al, 3; 80x25 text mode
+    int 10h
+
     mov cx, 0; will be used as character counter
     mov bx, buffer; will be used as buffer pointer
 
@@ -21,7 +27,7 @@ start:
     mov ah, 0
     int 16h
 
-    cmp al, BACKSPACE; if the character is backspace
+    cmp al, ESC; if the character is backspace
     je .call_handle_backspace; jump to handle_backspace
     cmp al, ENTER; if the character is enter
     je .call_handle_enter; jump to handle_enter
@@ -44,7 +50,6 @@ clear_screen:
     mov ah, 0; set the video mode
     mov al, 3; 80x25 text mode
     int 10h
-    ret
 
 handle_symbol:
     mov [bx], al; store the character in the buffer
@@ -65,20 +70,47 @@ handle_backspace:
     dec cx; decrement the character counter
     dec byte [cursor_x]; decrement the cursor x coordinate
     pusha; save all registers
-    mov ax, 1303H; print the character at the cursor position
-    mov bh, 0; page number
-    mov dx, cursor_coords; cursor coordinates
-    mov al, 0; print a space
-    int 10h
     mov ah, 02H; set the cursor position
     mov bh, 0; page number
-    mov dx, cursor_coords; cursor coordinates
+    mov dx, [cursor_coords]; cursor coordinates
+    int 10h
+
+    mov ah, 0AH; print the character at the cursor position
+    mov bh, 0; page number
+    mov cx, 2; number of times to print the character
+    mov al, ' '; print a space
     int 10h
     popa; restore all registers
     .backspace_done:
     ret
 
 handle_enter:
+    mov ax, 1300H; print string
+    mov bh, 0; page number
+    mov bl, 07H; text attribute
+    ; cx = number of characters to print
+    mov dh, [row];
+    mov dl, 0; column
+    mov bp, buffer; pointer to string
+    int 10h
+
+    mov byte [cursor_x], 0; set the cursor x coordinate to 0
+    inc byte [row]; increment the row number
+    mov bx, buffer; set the buffer pointer to the beginning of the buffer
+    xor cx, cx; set the character counter to 0
+    pusha; save all registers
+
+    mov ah, 02H; set the cursor position
+    mov bh, 0; page number
+    mov dx, [cursor_coords]; cursor coordinates
+    int 10h
+    ; clear line
+    mov ah, 0AH; print the character at the cursor position
+    mov bh, 0; page number
+    mov cx, 80; number of times to print the character
+    mov al, ' '; print a space
+    int 10h
+    popa; restore all registers
     ret
 
 times 510-($-$$) db 0
